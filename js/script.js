a2bc5969a9e87ebd0c4c9a7f7104c210687984a4
Yasin Karimzadeh <yasinkarimzadehgh@gmail.com>
2022-09-07 16:16:46 +0430

    modified

--- js/script.js
+++ js/script.js
@@ -4,12 +4,31 @@ const menMenu = document.querySelector('.men-menu');
 const menSubmenu = document.querySelector('.men-submenu');
 
 menMenu.addEventListener('mouseover', () => {
-  menSubmenu.style.top = "102%";
-  menSubmenu.style.opacity = "1";
+  menSubmenu.style.display = "inline-block";
 })
 menMenu.addEventListener('mouseout', () => {
-    menSubmenu.style.top = "200%";
-    menSubmenu.style.opacity = "0";
+  menSubmenu.style.display = "none";
+})
+//-------------------------------------------------------
+//! women-menu :
+const womenMenu = document.querySelector('.women-menu');
+const womenSubmenu = document.querySelector('.women-submenu');
 
+womenMenu.addEventListener('mouseover', () => {
+  womenSubmenu.style.display = "inline-block";
+})
+womenMenu.addEventListener('mouseout', () => {
+  womenSubmenu.style.display = "none";
+})
+//-------------------------------------------------------
+//! jewelry-menu :
+const jewelryMenu = document.querySelector('.jewelry-menu');
+const jewelrySubmenu = document.querySelector('.jewelry-submenu');
+
+jewelryMenu.addEventListener('mouseover', () => {
+  jewelrySubmenu.style.display = "inline-block";
+})
+jewelryMenu.addEventListener('mouseout', () => {
+  jewelrySubmenu.style.display = "none";
 })
 //-------------------------------------------------------
